7c3c902fc65d7965acf301c3ba344a39bc98fbc2
vhl232 <vhl232@gmail.com>
2019-04-07 20:03:49 +0300

    add data provider

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -2,10 +2,13 @@ public class Util {
 
     String webDriverFireFoxInstanse = "webdriver.gecko.driver";
     String locationFierfoxdriver = "D:\\geckodriver-v0.24.0-win64\\geckodriver.exe";
+    String localFirefoxDriver2 = "F:\\selenium drivera\\Mozilla GeckoDriver\\geckodriver-v0.24.0-win64\\geckodriver.exe";
 
     String urlGuru99 = "http://www.demo.guru99.com/V4/";
 
     String dataPathForGuru99 = "src/main/resources/Data.properties";
     String pathEXELFile = "src/main/resources/ForTest.xlsx";
     String guruHomeTitle = "Guru99 Bank Home Page";
+    String messegeFromAlert = "User or Password is not valid";
+    String manage_USER_ID = "Manger Id : mngr186603";
 }
---
+++ src/main/java/VerifyLoginWithDataProvider.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class VerifyLoginWithDataProvider {
+    WebDriver driver;
+    Util util = new Util();
+    PageLogin pageLogin;
+
+    @BeforeClass
+    public void setProperties(){
+        System.setProperty(util.webDriverFireFoxInstanse,util.localFirefoxDriver2);
+        driver = new FirefoxDriver();
+        pageLogin = new PageLogin(driver);
+    }
+    @Test(dataProvider = "dataFor_login",dataProviderClass = DataProviderClass.class )
+    public void verifyLogin_dataProvider(String userID,String password){
+        driver.navigate().to(util.urlGuru99);
+        pageLogin.getUserIDArea().sendKeys(userID);
+        pageLogin.getPasswArea().sendKeys(password);
+        pageLogin.getLoginButton().click();
+        if(util.guruHomeTitle.equalsIgnoreCase(driver.getTitle())){
+            WebElement table = driver.findElement(By.tagName("table"));
+            WebElement rowTable = table.findElement(By.xpath("tbody/tr/td/table/tbody/tr[3]/td"));
+            String manegeId = rowTable.getText();
+            System.out.println(manegeId);
+            Assert.assertEquals(manegeId,util.manage_USER_ID);
+        }
+    }
+
+
+}
