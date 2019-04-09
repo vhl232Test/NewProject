19aac5ade997cbe31720661b794c746198eb77df
vhl232Guru <vhl232@gmail.com>
2019-04-09 15:33:56 +0300

    screen

---
+++ src/main/java/DateQ.java
@@ -0,0 +1,24 @@
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateQ {
+
+
+       public static String  timeScreenShot(){
+           SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss");
+
+           String dateString = format.format( new Date());
+           System.out.println(dateString);
+           return dateString;
+
+       }
+
+
+    public static void main(String[] args) {
+        timeScreenShot();
+    }
+
+
+
+}
